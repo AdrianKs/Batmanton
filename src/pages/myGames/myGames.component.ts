13604c39592cba1a1cf602c4ed4c76373e855226
xqtion <andre-h96@gmx.de>
2016-12-11 16:06:33 +0100

    :

--- src/pages/myGames/myGames.component.ts
+++ src/pages/myGames/myGames.component.ts
@@ -2,13 +2,14 @@
  * Created by kochsiek on 08.12.2016.
  */
 import { Component } from '@angular/core';
-
+import { Platform } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'myGames.component.html'
 })
 export class MyGamesComponent {
+  gameStatus: string = "vergangende";
 
   constructor(public navCtrl: NavController) {
 
