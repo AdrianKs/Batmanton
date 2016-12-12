ea17e15746e5f493da1a31b714fb993777d9ea31
Manuel Entenmann <manuel.entenmann@gmail.com>
2016-12-12 20:08:31 +0100

    Edit Benutzerverwaltung, Spielerliste hinzugefuegt...

--- src/pages/userManagement/userManagement.component.ts
+++ src/pages/userManagement/userManagement.component.ts
@@ -11,8 +11,88 @@ import { NavController } from 'ionic-angular';
 })
 export class UserManagementComponent {
 
-  constructor(public navCtrl: NavController) {
+  geschlecht: string = "maenner";
+  testDataPlayer: any[];
+  testDataTeams: any[];
 
+  constructor(public navCtrl: NavController) {
+    this.initializeItems();
   }
 
+  initializeItems() {
+   /* this.testDataTeams = [
+      {
+        name: "Mannschaft auswählen"
+      },
+      {
+        name: "S1"
+      },
+      {
+        name: "S2"
+      },
+      {
+        name: "J1",
+      },
+      {
+        name: "J2"
+      },
+      {
+        name: "J3"
+      }
+    ]*/
+
+    this.testDataPlayer = [
+      {
+        vorname: "Max",
+        nachname: "Mustermann",
+        geburtstag: "19.12.1996",
+        mannschaft: "J2",
+        email: "max.mustermann@gmx.com",
+        geschlecht: "m"
+      },
+      {
+        vorname: "Marc",
+        nachname: "Mannmuster",
+        geburtstag: "12.19.1991",
+        mannschaft: "S2",
+        email: "marc.mannmuster@gmail.com",
+        geschlecht: "m"
+      },
+      {
+        vorname: "Maria",
+        nachname: "Müller",
+        geburtstag: "07.02.1990",
+        mannschaft: "S1",
+        email: "test@web.de",
+        geschlecht: 'w'
+      }
+    ]
+  }
+
+  openEditor(ev){
+    console.log("test");
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.testDataPlayer = this.testDataPlayer.filter((item) => {
+        return (item.nachname.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
+
+
+//Firebase Login
+//manuel-entenmann@web.de
+//Test123
+
+//Spielerliste mit Tabs
+
