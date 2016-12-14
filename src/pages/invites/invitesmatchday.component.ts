9d43fdc4c2ed7c74fe79f656dd6e4ff5d74fedaf
Lucas Groß-Hardt <lucas.gross-hardt@gmx.de>
2016-12-14 16:23:12 +0100

    test

---
+++ src/pages/invites/invitesmatchday.component.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by kochsiek on 08.12.2016.
+ */
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+import {ViewMatchdayComponent} from "../matchday/viewmatchday.component";
+
+@Component({
+  selector: 'page-invitesmatchday',
+  templateUrl: 'invitesmatchday.component.html'
+})
+export class InvitesMatchdayComponent {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+
+ goToPage(){
+    this.navCtrl.push(ViewMatchdayComponent);
+  }
+
+}
