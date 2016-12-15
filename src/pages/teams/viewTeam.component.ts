f9659f98554d5f7ea78a794713dee43b5767040a
Kipka <jan.kipka@hpe.com>
2016-12-15 11:42:13 +0100

    Weiter Funktionalitäten zu ViewTeam und EditTeam hinzugefügt

--- src/pages/teams/viewTeam.component.ts
+++ src/pages/teams/viewTeam.component.ts
@@ -3,7 +3,9 @@
  */
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, PopoverController } from 'ionic-angular';
+
+import { PopoverPage } from './popover.component';
 
 @Component({
   templateUrl: 'viewTeam.component.html'
@@ -15,14 +17,24 @@ export class ViewTeamComponent {
     playersOfTeam: any[];
     justPlayers = [];
 
-  constructor(public navCtrl: NavController, private navP: NavParams) {
+  constructor(public navCtrl: NavController, private navP: NavParams, public popoverCtrl: PopoverController ) {
       this.team = navP.get("team");
-      this.playersOfTeam = this.team.spieler;
+      this.playersOfTeam = this.team.players;
       for ( var key in this.playersOfTeam){
           if(this.playersOfTeam.hasOwnProperty(key)){
               this.justPlayers.push(this.playersOfTeam[key]);
           }
       }
+  }
+
+   presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(PopoverPage, 
+    {
+        value: this.team
+    });
+    popover.present({
+      ev: myEvent
+    });
   }
 
 
