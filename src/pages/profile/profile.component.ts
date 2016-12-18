4d8eae98e4a5a9e4f9494dd4adf04da9f6111c40
Sebastian Röhling <se.roehling@gmx.de>
2016-12-18 20:55:46 +0100

    implemented db connection

--- src/pages/profile/profile.component.ts
+++ src/pages/profile/profile.component.ts
@@ -5,28 +5,46 @@
 // change pb
 // db anbindung
 // Label-/ Inputausrichtungen?
-// Geschlecht?
-// passwort ändern
-// form Standalone Adrian
+// Geschlecht ändern?
+// passwort ändern?
+// Verein wählen
+// Einstellungsscreen (Benachrichtigungen, Verein ändern)
+// UserManagement --> DB Call auf Rollen begrenzen
+// Error Handling
 
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LoginComponent} from "../login/login.component";
 import {NavController, Button} from 'ionic-angular';
 import firebase from 'firebase';
 import {FormBuilder, Validators, FormControl} from '@angular/forms';
+import {loggedInUser} from "../../app/globalVars";
+import {AuthData} from '../../providers/auth-data';
 
 @Component({
   selector: 'page-profile',
-  templateUrl: 'profile.component.html'
+  templateUrl: 'profile.component.html',
+  providers: [AuthData]
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.getPlayer();
+  }
 
   public profileForm;
-  playerData: any;
+  loggedInUserID: string = loggedInUser.uid;
+  player: any;
   editMode: boolean = false;
+  /**
+   * Indicates wether the data for the view has been successfully loaded or not. If true, the profile-form in the template can be displayed
+   * @type {boolean}
+   */
+  dataLoaded: boolean = false;
   today: String = new Date().toISOString();
 
-  // Save values from form before entering edit view to check for changes
+  /**
+   *  Save values from form before entering edit view to check for changes
+   */
   firstnameOld: string;
   lastnameOld: string;
   emailOld: string;
@@ -41,9 +59,7 @@ export class ProfileComponent {
   birthdayChanged: boolean = false;
   teamChanged: boolean = false;
 
-  constructor(public navCtrl: NavController, public formBuilder: FormBuilder,) {
-    this.initializeItems();
-
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public authData: AuthData,) {
     this.profileForm = formBuilder.group({
       firstname: ['', Validators.compose([Validators.required, Validators.minLength(2), this.startsWithACapital])],
       lastname: ['', Validators.compose([Validators.required, Validators.minLength(2), this.startsWithACapital])],
@@ -53,54 +69,50 @@ export class ProfileComponent {
     })
   }
 
-  initializeItems() {
-    this.playerData =
-      {
-        birthday: "1995-07-21",
-        email: "se.roehling@gmail.com",
-        firstname: "Sebastian",
-        gender: "m",
-        isPlayer: true,
-        isTrainer: false,
-        lastname: "Röhling",
-        pushid: "",
-        state: 0,
-        team: "15"
-
-      };
+  /**
+   * Gets the data for the logged in player from the database and sets the "dataLoaded" flag afterwards "true".
+   */
+  getPlayer(): void {
+    firebase.database().ref('clubs/12/players/' + this.loggedInUserID).once('value', snapshot => {
+      this.player = snapshot.val();
+      this.dataLoaded = true;
+    })
   }
 
   /**
    * Formats the birthday coming from the player data value to "dd.mm.yyyy" to display it with a label
    * @returns {string} Formatted birthday
    */
-  formatBirthday(){
-    return this.playerData.birthday.split("-")[2] + "." + this.playerData.birthday.split("-")[1] + "." + this.playerData.birthday.split("-")[0];
+  formatBirthday() {
+    return this.player.birthday.split("-")[2] + "." + this.player.birthday.split("-")[1] + "." + this.player.birthday.split("-")[0];
   }
 
   editProfile() {
-    this.firstnameOld = this.playerData.firstname;
-    this.lastnameOld = this.playerData.lastname;
-    this.emailOld = this.playerData.email;
-    this.birthdayOld = this.playerData.birthday;
-    this.teamOld = this.playerData.team;
-    console.log("Firstname " + this.firstnameOld);
-    console.log("Lastname " + this.lastnameOld);
-    console.log("Email " + this.emailOld);
-    console.log("Birthday " + this.birthdayOld);
-    console.log("Team " + this.teamOld);
+    this.firstnameOld = this.player.firstname;
+    this.lastnameOld = this.player.lastname;
+    this.emailOld = this.player.email;
+    this.birthdayOld = this.player.birthday;
+    this.teamOld = this.player.team;
     this.editMode = true;
   }
 
   finishEditProfile() {
-    console.log("Model:")
-    console.log("Firstname " + this.playerData.firstname);
-    console.log("Lastname " + this.playerData.lastname);
-    console.log("Email " + this.playerData.email);
-    console.log("Birthday " + this.playerData.birthday);
-    console.log("Team " + this.playerData.team);
     if (this.firstnameOld || this.lastnameChanged || this.emailChanged || this.birthdayChanged || this.teamChanged) {
-      console.log("do DB Op");
+      firebase.database().ref('clubs/12/players/' + this.loggedInUserID).set({
+        birthday: this.player.birthday,
+        email: this.player.email,
+        firstname: this.player.firstname,
+        gender: this.player.gender,
+        isTrainer: this.player.isTrainer,
+        isPlayer: this.player.isPlayer,
+        lastname: this.player.lastname,
+        pushid: this.player.pushid,
+        state: this.player.state,
+        team: this.player.team
+      });
+    }
+    if (this.emailChanged) {
+      this.authData.changeEmail(this.player.email);
     }
     this.firstnameChanged = false;
     this.lastnameChanged = false;
