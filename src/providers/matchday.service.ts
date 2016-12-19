8564545f6a31864b6d7f84fc270d5ff21cb57743
xqtion <andre-h96@gmx.de>
2016-12-19 14:28:13 +0100

    Service TS Files added

---
+++ src/providers/matchday.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class MatchdayService {
+
+    playerData: Array<any>;
+
+    constructor(private http: Http) {
+        console.log('Task Service Initialized...');
+    }
+
+    getPlayers() {
+        return this.http.get("https://batmanton-f8643.firebaseio.com/clubs/12/players.json")
+            .map(res => res.json());
+    }
+}
