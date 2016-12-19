dd881f34ec4b4ec6ccde2a59755ce78baf062fc8
Kipka <jan.kipka@hpe.com>
2016-12-19 21:28:57 +0100

    Minor changes

--- src/pages/teams/editTeam.component.ts
+++ src/pages/teams/editTeam.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
+
+import firebase from 'firebase';
 
 
 @Component({
     templateUrl: 'editTeam.component.html'
 })
-export class EditTeamComponent{
+export class EditTeamComponent implements OnInit{
 
     team: any;
     justPlayers: any[];
     geschlecht: string = "maenner";
+    database: any;
+
+    ngOnInit(): void {
+    this.database = firebase.database();
+    }
 
     constructor(public nav: NavController, public nParam: NavParams){
         this.team = nParam.get("value");
@@ -19,6 +26,7 @@ export class EditTeamComponent{
     }   
 
     removePlayer(player: any){
-        
+        //REMOVE PLAYER
     }
+    
 }
