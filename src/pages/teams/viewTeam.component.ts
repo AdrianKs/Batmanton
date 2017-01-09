8b560fe26895956a7dab1c4a6dceab32531a2e20
Kipka <jan.kipka@hpe.com>
2017-01-09 15:00:43 +0100

    Design, Bugfixes, EditTeam Design geändert

--- src/pages/teams/viewTeam.component.ts
+++ src/pages/teams/viewTeam.component.ts
@@ -4,6 +4,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, PopoverController } from 'ionic-angular';
 import { PopoverPage } from './popover.component';
+import { EditTeamComponent } from './editTeam.component';
 import firebase from 'firebase';
 
 
@@ -19,15 +20,11 @@ export class ViewTeamComponent {
 
     team: any;
     geschlecht: string = "maenner";
-    //allPlayers : any;
     playersOfTeam: any[];
     justPlayers: any;
+    justPlayersPlaceholder: any;
     database: any;
-
-
-
-
-
+    editMode: boolean = false;
 
 
     constructor(public navCtrl: NavController, private navP: NavParams, public popoverCtrl: PopoverController) {
@@ -36,53 +33,27 @@ export class ViewTeamComponent {
         this.team = navP.get("team");
         console.log("TEAM IN VIEWTEAM VIEW");
         console.log(this.team);
-        this.justPlayers = this.team.players;
+        this.justPlayersPlaceholder = this.team.players;
+        this.checkIfUndefined();
+        
+    }
+
+    checkIfUndefined(){
+        for(let i in this.justPlayersPlaceholder){
+            let player = this.justPlayersPlaceholder[i];
+            if(player!=undefined){
+                this.justPlayers.push(player);
+            }
+        }
         console.log("JUST PLAYERS ARRAY:");
         console.log(this.justPlayers);
     }
 
-    fetchAllPlayers() {
-
-        this.justPlayers = this.team.players;
-        console.log("PLAYERS IN TEAM VIEW");
-        console.log(this.justPlayers);
-
-
-
-
-
-        
-
-        /*this.database.ref("/clubs/12/players/").once('value', snapshot => {
-            let allPlayers = snapshot.val();
-            this.findPlayersOfTeam(allPlayers);
-            //this.allPlayers = snapshot.val();
-            //Wie kann man auf die ID im Snapshot zugreifen?
-            let playerArray = [];
-            let counter = 0;
-            for (let y in this.justPlayers) { 
-            for (let i in snapshot.val()) {
-                let id = this.justPlayers.id;
-                let id_snap = snapshot.val()[0];
-                console.log("SNAPSHOT KEY: "+ id_snap);
-                if (id == id_snap) {
-                    playerArray[counter] = snapshot.val()[i];
-                    playerArray[counter].id = i;
-                }
-                counter++;
-            }
-        }*/
-        //})
-
-    }
-
-    findPlayersOfTeam(allPlayerArray: any) {
-        for (let j in this.playersOfTeam) {
-            let playerID = this.playersOfTeam[j];
-            for (let i in allPlayerArray) {
-
-            }
-        }
+    editTeam(){
+        this.editMode = true;
+        /*this.navCtrl.push(EditTeamComponent, {
+            value: this.team
+        })*/
     }
 
     presentPopover(myEvent) {
