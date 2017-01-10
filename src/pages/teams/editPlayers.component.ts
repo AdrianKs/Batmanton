60e82d2f36c8cb01b52ae243a5263fdfabd31b41
Kipka <jan.kipka@hpe.com>
2017-01-10 14:57:37 +0100

    ViewTeam und EditPlayer Komponenten angepasst, kleinere Bugfixes

--- src/pages/teams/editPlayers.component.ts
+++ src/pages/teams/editPlayers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Utilities } from '../../app/utilities';
 import firebase from 'firebase';
 
 
@@ -11,34 +11,50 @@ import firebase from 'firebase';
 export class EditPlayerComponent implements OnInit {
 
     team: string = "";
+    teamId: string = "";
     spielerStatus: string = "chosen";
     allPlayers: any[];
+    allPlayersMod: any[];
+    allPlayersModSearch: any[];
     allPlayersSearch: any[];
     geschlecht: string = "maenner";
     database: any;
     teams: any[];
+    playersOfTeamPlaceholder: any;
     playersOfTeam: any;
+    playersOfTeamSearch: any;
 
     ngOnInit(): void {
-        this.database = firebase.database();
-        this.getPlayers();
-        this.getTeams();
     }
 
-    constructor(public nav: NavController, public nParam: NavParams) {
-        this.playersOfTeam = nParam.get("param");
+    constructor(public nav: NavController, public nParam: NavParams, public utilities: Utilities) {
+        this.database = firebase.database();
+        this.playersOfTeam = [];
+        //NULL ABFRAGEN EINBAUEN
+        this.playersOfTeamPlaceholder = nParam.get("param");
+        this.teamId = nParam.get("teamId");
+        this.getPlayers();
+        this.getTeams();
+        this.checkIfUndefined();
     }
 
     initializePlayers() {
         this.allPlayers = this.allPlayersSearch;
+        this.playersOfTeam = this.playersOfTeamSearch;
     }
 
-    getPlayers(){
+    getPlayers() {
         this.database.ref("/clubs/12/players/").once('value', snapshot => {
             let playerArray = [];
+            let completePlayerArray = snapshot.val();
+            //console.log("Cmplete Player Array");
+            //console.log(completePlayerArray);
+            let uniqueId = 0;
             let counter = 0;
-            for (let i in snapshot.val()) {
+            for (let i in completePlayerArray) {
+                //console.log(i);
                 playerArray[counter] = snapshot.val()[i];
+                playerArray[counter].uniqueId = i;
                 counter++;
             }
             this.allPlayers = playerArray;
@@ -47,12 +63,35 @@ export class EditPlayerComponent implements OnInit {
         });
     }
 
-    getPlayerArray(){
+    getPlayerArray() {
         return this.allPlayers;
     }
 
+    doubleCheckLists() {
+        let id1 = "";
+        let id2 = "";
+        let player;
+        let player2;
+        for (let i in this.playersOfTeam) {
+            for (let y in this.allPlayers) {
+                console.log("Geht rein");
+                player = this.playersOfTeam[i];
+                id1 = player.uniqueId;
+                console.log("ID1 " + id1);
+                player2 = this.allPlayers[y];
+                console.log("auch hier geht er rein");
+                id2 = player2.uniqueId;
+                if (id1 != id2) {
+                    //Aktueller Spieler ist nicht ausgewählt, also hinzufügen
+                    this.allPlayersMod.push(player2);
+                } else {
+                    //Spieler gefunden, aus Schleife raus
+                }
+            }
+        }
+    }
 
-    teamSelectChanged(team: any){
+    teamSelectChanged(team: any) {
         //let val = ev.target.value;
         console.log("VALUE: " + team);
         let playersLocal = this.getPlayerArray();
@@ -99,8 +138,63 @@ export class EditPlayerComponent implements OnInit {
         }
     }
 
-    removePlayer(p: any){
-        
+    removePlayer(p: any) {
+        this.database.ref('clubs/12/teams/' + this.teamId + '/players/' + p.id).remove();
+        this.getPlayersOfTeam();
+    }
+
+    addPlayer(p: any) {
+        this.utilities.addPlayerToTeam(this.teamId, p.uniqueId);
+    }
+
+    getPlayersOfTeam() {
+        this.database.ref("/clubs/12/teams/" + this.teamId + "/players/").once('value', snapshot => {
+            this.playersOfTeamPlaceholder = snapshot.val();
+            this.checkIfUndefined();
+        })
+    }
+
+    checkIfUndefined() {
+        for (let i in this.playersOfTeamPlaceholder) {
+            let player = this.playersOfTeamPlaceholder[i];
+            if (player != undefined && player != null) {
+                this.playersOfTeam.push(player);
+            }
+        }
+        this.playersOfTeamSearch = this.playersOfTeam;
+        //this.doubleCheckLists();
+        /*console.log("JUST PLAYERS ARRAY:");
+        console.log(this.playersOfTeam);*/
+    }
+
+    getItemsAvailable(ev) {
+        // Reset items back to all of the items
+        this.initializePlayers();
+
+        // set val to the value of the ev target
+        var val = ev.target.value;
+
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+            this.allPlayers = this.allPlayers.filter((item) => {
+                return (item.lastname.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            })
+        }
+    }
+
+    getItemsChosen(ev) {
+        // Reset items back to all of the items
+        this.initializePlayers();
+
+        // set val to the value of the ev target
+        var val = ev.target.value;
+
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+            this.playersOfTeam = this.playersOfTeam.filter((item) => {
+                return (item.lastname.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            })
+        }
     }
 
 
--- src/pages/teams/teams.component.ts
+++ src/pages/teams/teams.component.ts
@@ -28,14 +28,7 @@ export class TeamsComponent implements OnInit {
 
   constructor(public navCtrl: NavController, public fbP: FirebaseProvider, public utilities: Utilities) {
     this.database = firebase.database();
-    //this.getAllTeamData();
   }
-
-
-  /*testGetData() {
-    var test = this.fbP.getItemsOfRefOn("/clubs/12/teams/");
-    console.log(test);
-  }*/
 
 
   viewTeam(ev, value) {
@@ -49,149 +42,38 @@ export class TeamsComponent implements OnInit {
 
   getAllTeamData() {
     let playerPlaceholder = [];
-      let counter = 0;
-      for (let y in this.teams) {
-        playerPlaceholder[counter] = this.teams[y].players;
-        counter++;
-      }
-      console.log("PRINT BEFORE TEST");
-      this.playerArray = playerPlaceholder;
-      this.addPlayersToArray(this.playerArray);
-
-    //this.database.ref("/clubs/12/teams/").once('value', snapshot => {
-      /*console.log(snapshot.val());
-      let teamArray = [];
-      let counter = 0;
-      for (let i in snapshot.val()) {
-        teamArray[counter] = snapshot.val()[i];
-        teamArray[counter].id = i;
-        counter++;
-      }
-      this.teamsSearch = teamArray;
-      this.teams = teamArray;
-      let playerPlaceholder = [];
-      counter = 0;
-      for (let y in this.teams) {
-        playerPlaceholder[counter] = this.teams[y].players;
-        counter++;
-      }
-      console.log("PRINT BEFORE TEST");
-      this.playerArray = playerPlaceholder;
-      this.addPlayersToArray(this.playerArray);*/
-      /* console.log(playerPlaceholder);
-       counter = 0;
-       let playerIDs = [];
-       for (let j in playerPlaceholder){
-         let test = playerPlaceholder[j][counter];
-         console.log("ID: " + test);
-         playerIDs.push(test);
-         counter++;
-       }
-       console.log("PLAYER IDs");
-       console.log(playerIDs);*/
-    //});
-
-
-    //this.teams = this.fbP.getItemsOfRefOn("/clubs/12/teams/");
+    let counter = 0;
+    for (let y in this.teams) {
+      playerPlaceholder[counter] = this.teams[y].players;
+      counter++;
+    }
+    this.playerArray = playerPlaceholder;
+    this.addPlayersToArray(this.playerArray);
   }
 
   addPlayersToArray(valueArray: any) {
     this.database.ref("/clubs/12/players/").once('value', snapshot => {
-      console.log("VALUE ARRAY");
-      console.log(valueArray);
-      let allPlayers = snapshot.val();
       let idPlaceholder = 0;
       let counter = 0;
       for (let i in valueArray) {
         for (let y in valueArray[i]) {
           idPlaceholder = valueArray[i][y];
           let player = snapshot.child("" + idPlaceholder).val();
-          if (player != null) {
-            console.log("PLAYER: ");
-            console.log(player);
+          if ((player != null) && (player != undefined)) {
             this.teams[i].players[y] = player;
             this.teams[i].players[y].id = y;
+            this.teams[i].players[y].uniqueId = idPlaceholder;
           } else {
             console.log("Spieler übersprungen, da null");
           }
         }
       }
-      console.log("TEAMS:");
-      console.log(this.teams);
     })
 
   }
 
   initializeTeams() {
     this.teams = this.teamsSearch;
-    /*this.teams = [
-      {
-        isAdult: true,
-        name: "J1",
-        type: "0",
-        players: {
-          15: {
-            name: 'Thomas Test',
-            geschlecht: 'm',
-            geburtstag: '01.01.1996'
-          },
-          16: {
-            name: 'Tina Turner',
-            geschlecht: 'w',
-            geburtstag: '02.02.1997'
-          },
-          17: {
-            name: 'Tim Turner',
-            geschlecht: 'm',
-            geburtstag: '02.02.1997'
-          }
-        }
-      },
-      {
-        isAdult: false,
-        name: "J2",
-        type: "0",
-        players: {
-          15: {
-            name: 'Jonas Friedrich',
-            geschlecht: 'm',
-            geburtstag: '01.01.1996'
-          },
-          16: {
-            name: 'Lisa Albert',
-            geschlecht: 'w',
-            geburtstag: '02.02.1997'
-          },
-          17: {
-            name: 'Stefan Knolle',
-            geschlecht: 'm',
-            geburtstag: '02.02.1997'
-          }
-        }
-      },
-      {
-        isAdult: true,
-        name: "J3",
-        type: "0",
-        players: {
-          15: {
-            name: 'Tim Turbo',
-            geschlecht: 'm',
-            geburtstag: '01.01.1996'
-          },
-          16: {
-            name: 'Christina Kralle',
-            geschlecht: 'w',
-            geburtstag: '02.02.1997'
-          },
-          17: {
-            name: 'ASAP Ferg',
-            geschlecht: 'm',
-            geburtstag: '02.02.1997'
-          }
-        }
-      }
-    ]*/
   }
 
   getItems(ev) {
