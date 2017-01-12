b329a16e618b3ce66c25a2aa627bee53ccf52789
Kipka <jan.kipka@hpe.com>
2017-01-12 23:41:51 +0100

    UNSTABLE: Added Choose Picture Function

--- src/pages/teams/viewTeam.component.ts
+++ src/pages/teams/viewTeam.component.ts
@@ -10,7 +10,8 @@ import { EditPlayerComponent } from './editPlayers.component';
 import firebase from 'firebase';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { document } from "@angular/platform-browser/src/facade/browser";
-import {Camera} from 'ionic-native';
+import { Camera } from 'ionic-native';
+import { EditRoleComponent } from '../editRole/editRole.component';
 
 
 @Component({
@@ -37,13 +38,18 @@ export class ViewTeamComponent implements OnInit {
     formValid: boolean = false;
     actionSheetOptions: any;
     teamPicUrl: any;
-
+    teamName: any;
+    altersK: any;
+    altersBez: any;
+    currentUser: any;
+    currentUserID: any;
+    isAdmin: boolean;
     /**
      * OLD VALUES
      */
     teamNameOld: string;
     altersKOld: any;
-    altersBezOld: string;
+    altersBezOld: any;
 
     base64Image: string;
     base64String: string;
@@ -51,21 +57,40 @@ export class ViewTeamComponent implements OnInit {
 
 
 
+    /**
+     * Diese Methode wird beim erstmaligen Initialisieren der Komponente aufgerufen.
+     */
     ngOnInit(): void {
+        
         this.database = firebase.database();
         this.teamId = this.navP.get("teamId");
         this.initActionSheet();
         //this.getTeamPicture();
     }
 
-    ionViewWillEnter(){
+    /**
+     * Diese Methode wird verwendet, um die Rolle des aktuellen Benutzers abzufragen.
+     */
+    isTrainer() {
+        this.currentUser = this.utils.user;
+        this.database.ref("/clubs/12/players/" + this.currentUser.uid + "/").once('value', snapshot => {
+            let data = snapshot.val();
+            this.isAdmin = data.isTrainer;
+        })
+    }
+
+    /**
+     * Diese Methode wird jedes Mal ausgeführt, sobald die View aufgebaut wird und sorgt so für den Refresh der Daten.
+     */
+    ionViewWillEnter() {
+        this.isTrainer();
         this.justPlayers = [];
         this.buildTeam();
     }
 
 
     //TODO: Probieren ob ionViewLoaded() noch hilft um View zu refreshen
-    ionViewDidEnter(){
+    ionViewDidEnter() {
         /*this.justPlayers = [];
         this.buildTeam();*/
     }
@@ -86,7 +111,7 @@ export class ViewTeamComponent implements OnInit {
             altersK: []
         })
 
-        
+
 
         //this.teamId = this.navP.get("teamId");
         //console.log(this.teamId);
@@ -104,6 +129,14 @@ export class ViewTeamComponent implements OnInit {
     buildTeam() {
         this.database.ref("/clubs/12/teams/" + this.teamId + "/").once('value', snapshot => {
             this.team = snapshot.val();
+            this.teamNameOld = this.team.name;
+            this.altersKOld = this.team.ageLimit;
+            this.altersBezOld = this.team.type;
+            if (this.altersBezOld == 1) {
+                this.altersBezOld = "Mini";
+            } else {
+                this.altersBezOld = "Erwachsen";
+            }
             console.log(this.team);
             //this.team = snapshot.val();
             if (this.team != null && this.team != undefined) {
@@ -121,37 +154,77 @@ export class ViewTeamComponent implements OnInit {
         })
     }
 
+    viewPlayer(p: any){
+        this.navCtrl.push(EditRoleComponent, {
+            player: p
+        })
+    }
 
-    updateTeamInfos() {
-        if (this.validate()) {
-            let teamName = this.teamForm.value.teamName;
-            let altersK = this.teamForm.value.altersK;
-            let altersBez = this.teamForm.value.altersBez;
-            if (altersBez == "Mini") {
-                altersBez = 1;
-            } else if (altersBez == "Erwachsen") {
-                altersBez = 0;
+
+    updateTeamInfos(infosChanged: boolean) {
+        if (infosChanged) {
+            if (this.altersBez == "Mini") {
+                this.altersBez = 1;
+            } else if (this.altersBez == "Erwachsen") {
+                this.altersBez = 0;
             } else {
                 //besser erwachsen als mini
-                altersBez = 0;
+                this.altersBez = 0;
             }
 
             //UPDATE Database
-            firebase.database().ref('clubs/12/teams/' + this.teamId).set({
-                ageLimit: altersK,
-                name: teamName,
-                type: altersBez
+            firebase.database().ref('clubs/12/teams/' + this.teamId).update({
+                ageLimit: this.altersK,
+                name: this.teamName,
+                type: this.altersBez
             });
 
             //UPDATE Array 
-            this.team.ageLimit = altersK;
-            this.team.name = teamName;
-            this.team.type = altersBez;
+            this.team.ageLimit = this.altersK;
+            this.team.name = this.teamName;
+            this.team.type = this.altersBez;
 
             console.log(this.team);
-            //END Edit Mode
-            this.editMode = false;
+
         }
+        //END Edit Mode
+        this.editMode = false;
+    }
+
+    getFormData() {
+
+        this.teamName = this.teamForm.value.teamName;
+        this.altersK = this.teamForm.value.altersK;
+        this.altersBez = this.teamForm.value.altersBez;
+        if (this.teamName == null) {
+            //Keine Änderung
+            this.teamNameChanged = false;
+            this.teamName = this.teamNameOld;
+        } else {
+            this.teamNameChanged = true;
+        }
+        if (this.altersK == null) {
+            //Keine Änderung
+            this.altersKChanged = false;
+            this.altersK = this.altersKOld;
+        } {
+            this.altersKChanged = true;
+        }
+        if (this.altersBez == null) {
+            this.altersBezChanged = false;
+            this.altersBez = this.altersBezOld;
+        } {
+            this.altersBezChanged = true;
+        }
+        if ((!this.altersKChanged) && (!this.altersBezChanged) && (!this.teamNameChanged)) {
+            //Keine Änderung
+            this.updateTeamInfos(false);
+        } else {
+            this.updateTeamInfos(true);
+        }
+        /*console.log(this.teamName);
+        console.log(this.altersK);
+        console.log(this.altersBez);*/
     }
 
     validate() {
@@ -199,16 +272,21 @@ export class ViewTeamComponent implements OnInit {
             let idPlaceholder = 0;
             let counter = 0;
             for (let i in valueArray) {
+                //ID als String
                 idPlaceholder = valueArray[i];
                 let player = snapshot.child("" + idPlaceholder).val();
                 if ((player != null) && (player != undefined)) {
-                    this.justPlayers[i] = player;
+                    this.justPlayers.push(player);
+                    this.justPlayers[counter].id =i;
+                    this.justPlayers[counter].uniqueId =idPlaceholder;
+                    /*this.justPlayers[i] = player;
                     this.justPlayers[i].id = i;
-                    this.justPlayers[i].uniqueId = idPlaceholder;
+                    this.justPlayers[i].uniqueId = idPlaceholder;*/
                     /*this.teams[i].players[y] = player;
                     this.teams[i].players[y].id = y;
                     this.teams[i].players[y].uniqueId = idPlaceholder;*/
                 }
+                counter++;
             }
             console.log(this.justPlayers);
             /*for (let i in valueArray) {
@@ -226,6 +304,14 @@ export class ViewTeamComponent implements OnInit {
             }*/
         })
 
+    }
+
+    handleClick(action:any, p: any){
+        if(action="delete"){
+            this.presentConfirm(p, "delP");
+        }else if(action=="view"){
+            this.viewPlayer(p);
+        }
     }
 
     getTeam() {
@@ -289,12 +375,19 @@ export class ViewTeamComponent implements OnInit {
         let uniqueId = 0;
         let deleteId = p.uniqueId;
         this.justPlayers = [];
+        let counter = 0;
         for (let i in placeHolderArray) {
-            uniqueId = placeHolderArray[i].uniqueId;
+            let player = placeHolderArray[i];
+            uniqueId = player.uniqueId;
+            //console.log(uniqueId);
             if (uniqueId != deleteId) {
-                this.justPlayers.push(placeHolderArray[i]);
+                this.justPlayers.push(player);
+                this.justPlayers[counter].id = i;
+                counter++;
             }
         }
+        console.log("Just Players in Delete Method");
+        console.log(this.justPlayers);
 
         this.database.ref('clubs/12/teams/' + this.teamId + '/players/' + p.id).remove();
         this.database.ref('clubs/12/players/' + p.uniqueId + '/').update({
@@ -314,17 +407,15 @@ export class ViewTeamComponent implements OnInit {
             this.deleteTeamFromPlayer(p.id, idToBeDeleted);
             }
         })*/
-       
+
         this.playerDeleted = true;
         //this.getPlayersOfTeamDb();
         //this.getPlayersOfTeam();
     }
 
-
-    deleteTeamFromPlayer(playerId: any, teamIdToBeDeleted: any){
-         this.database.ref('clubs/12/players/' + playerId + '/teams/' + teamIdToBeDeleted).remove();
+    deleteTeamFromPlayer(playerId: any, teamIdToBeDeleted: any) {
+        this.database.ref('clubs/12/players/' + playerId + '/teams/' + teamIdToBeDeleted).remove();
     }
-
 
     deleteTeam() {
         this.database.ref('clubs/12/teams/' + this.teamId).remove();
@@ -399,18 +490,18 @@ export class ViewTeamComponent implements OnInit {
     }
 
     callCamera(options) {
-    Camera.getPicture(options)
-      .then((imageData) => {
-        // imageData is a base64 encoded string
-        this.base64Image = "data:image/jpeg;base64," + imageData;
-        this.base64String = imageData;
-        this.uploadPicture();
-      }, (err) => {
-        console.log(err);
-      });
-  }
+        Camera.getPicture(options)
+            .then((imageData) => {
+                // imageData is a base64 encoded string
+                this.base64Image = "data:image/jpeg;base64," + imageData;
+                this.base64String = imageData;
+                this.uploadPicture();
+            }, (err) => {
+                console.log(err);
+            });
+    }
 
-  makeid() {
+    makeid() {
         var text = "";
         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
@@ -420,41 +511,41 @@ export class ViewTeamComponent implements OnInit {
         return text;
     }
 
-  uploadPicture() {
-      let id = this.makeid();
-      this.teamPicUrl = id;
-    // firebase.storage().ref().child('profilePictures/' + this.utilities.userDataID + "/" + this.utilities.userDataID + ".jpg").putString(this.base64String, 'base64', {contentType: 'image/JPEG'})
-    firebase.storage().ref().child('teamPictures/' + id+ ".jpg").putString(this.base64String, 'base64', {contentType: 'image/JPEG'})
-      .then(callback => {
-        document.getElementById("teamPic").src = this.base64Image;
-        this.storeUrl(callback.downloadURL);
-        // Depending on whether an image is uploaded or not, display the delete image option in the action sheet or not
-        this.actionSheetOptions = {
-          title: 'Profilbild ändern',
-          buttons: [
-            {
-              text: 'Fotos',
-              icon: "images",
-              handler: () => this.getPicture()
-            },
-            {
-              text: 'Profilbild löschen',
-              role: 'destructive',
-              icon: "trash",
-              handler: () => this.deleteTeamPicture()
-            },
-            {
-              text: 'Abbrechen',
-              role: 'cancel'
-            }
-          ]
-        };
-      })
-      .catch(function (error) {
-        alert(error.message);
-        console.log(error);
-      });
-  }
+    uploadPicture() {
+        let id = this.makeid();
+        this.teamPicUrl = id;
+        // firebase.storage().ref().child('profilePictures/' + this.utilities.userDataID + "/" + this.utilities.userDataID + ".jpg").putString(this.base64String, 'base64', {contentType: 'image/JPEG'})
+        firebase.storage().ref().child('teamPictures/' + id + ".jpg").putString(this.base64String, 'base64', { contentType: 'image/JPEG' })
+            .then(callback => {
+                document.getElementById("teamPic").src = this.base64Image;
+                this.storeUrl(callback.downloadURL);
+                // Depending on whether an image is uploaded or not, display the delete image option in the action sheet or not
+                this.actionSheetOptions = {
+                    title: 'Profilbild ändern',
+                    buttons: [
+                        {
+                            text: 'Fotos',
+                            icon: "images",
+                            handler: () => this.getPicture()
+                        },
+                        {
+                            text: 'Profilbild löschen',
+                            role: 'destructive',
+                            icon: "trash",
+                            handler: () => this.deleteTeamPicture()
+                        },
+                        {
+                            text: 'Abbrechen',
+                            role: 'cancel'
+                        }
+                    ]
+                };
+            })
+            .catch(function (error) {
+                alert(error.message);
+                console.log(error);
+            });
+    }
 
     changeTeamPicture() {
         let actionSheet = this.actionSheetCtrl.create(this.actionSheetOptions);
@@ -466,45 +557,19 @@ export class ViewTeamComponent implements OnInit {
     deleteTeamPicture() {
         var that = this;
         this.teamPicUrl = this.team.teamPicId;
-    // firebase.storage().ref().child('profilePictures/test.jpg').delete().then(function() {
-    // firebase.storage().ref().child('profilePictures/' + this.utilities.userDataID + "/" + this.utilities.userDataID + '.jpg').delete().then(function () {
-    firebase.storage().ref().child('teamPictures/' + this.teamPicUrl+ '.jpg').delete().then(function () {
-      document.getElementById("teamPic").src = "assets/images/ic_account_circle_black_48dp_2x.png";
+        // firebase.storage().ref().child('profilePictures/test.jpg').delete().then(function() {
+        // firebase.storage().ref().child('profilePictures/' + this.utilities.userDataID + "/" + this.utilities.userDataID + '.jpg').delete().then(function () {
+        firebase.storage().ref().child('teamPictures/' + this.teamPicUrl + '.jpg').delete().then(function () {
+            document.getElementById("teamPic").src = "assets/images/ic_account_circle_black_48dp_2x.png";
 
-      // Depending on whether an image is uploaded or not, display the delete image option in the action sheet or not
-      that.actionSheetOptions = {
-        title: 'Profilbild ändern',
-        buttons: [
-          {
-            text: 'Fotos',
-            icon: "images",
-            handler: () => that.getPicture()
-          },
-          {
-            text: 'Abbrechen',
-            role: 'cancel'
-          }
-        ]
-      };
-    }).catch(function (error) {
-      alert(error.message);
-    });
-    }
-
-    initActionSheet(){
-        this.actionSheetOptions = {
-                title: 'Teambild ändern',
+            // Depending on whether an image is uploaded or not, display the delete image option in the action sheet or not
+            that.actionSheetOptions = {
+                title: 'Profilbild ändern',
                 buttons: [
                     {
                         text: 'Fotos',
                         icon: "images",
-                        handler: () => this.getPicture()
-                    },
-                    {
-                        text: 'Profilbild löschen',
-                        role: 'destructive',
-                        icon: "trash",
-                        handler: () => this.deleteTeamPicture()
+                        handler: () => that.getPicture()
                     },
                     {
                         text: 'Abbrechen',
@@ -512,12 +577,38 @@ export class ViewTeamComponent implements OnInit {
                     }
                 ]
             };
+        }).catch(function (error) {
+            alert(error.message);
+        });
+    }
+
+    initActionSheet() {
+        this.actionSheetOptions = {
+            title: 'Teambild ändern',
+            buttons: [
+                {
+                    text: 'Fotos',
+                    icon: "images",
+                    handler: () => this.getPicture()
+                },
+                {
+                    text: 'Profilbild löschen',
+                    role: 'destructive',
+                    icon: "trash",
+                    handler: () => this.deleteTeamPicture()
+                },
+                {
+                    text: 'Abbrechen',
+                    role: 'cancel'
+                }
+            ]
+        };
     }
 
     getTeamPicture() {
         var that = this;
         // firebase.storage().ref().child("profilePictures/" + this.utilities.userDataID + "/" + this.utilities.userDataID + ".jpg").getDownloadURL().then(function (url) {
-        firebase.storage().ref().child("teamPictures/" + this.teamId+"_"+this.team.name + "/" + this.teamId +"_"+this.team.name+ ".jpg").getDownloadURL().then(function (url) {
+        firebase.storage().ref().child("teamPictures/" + this.teamId + "_" + this.team.name + "/" + this.teamId + "_" + this.team.name + ".jpg").getDownloadURL().then(function (url) {
             document.getElementById("teamPic").src = url;
             // Depending on whether an image is uploaded or not, display the delete image option in the action sheet or not
             that.actionSheetOptions = {
@@ -561,17 +652,11 @@ export class ViewTeamComponent implements OnInit {
     }
 
     storeUrl(url: string) {
-    firebase.database().ref('clubs/12/teams/' + this.teamId).update({
-        teamPicUrl: url,
-        teamPicId: this.teamPicUrl
-    });
-  }
+        firebase.database().ref('clubs/12/teams/' + this.teamId).update({
+            teamPicUrl: url,
+            teamPicId: this.teamPicUrl
+        });
+    }
 
 
 }
-
-
-
-
-
-
