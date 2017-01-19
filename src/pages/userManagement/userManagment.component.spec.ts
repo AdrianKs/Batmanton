8552cb2539cf347767e79a5ec884d34aff27c754
Manuel Entenmann <manuel.entenmann@gmail.com>
2017-01-19 20:06:34 +0100

    Set up new testframework..

---
+++ src/pages/userManagement/userManagment.component.spec.ts
@@ -0,0 +1,18 @@
+import {UserManagementComponent} from './userManagement.component';
+import { NavController, NavParams } from 'ionic-angular';
+import { Utilities } from '../../app/utilities';
+
+let userManagement: UserManagementComponent;
+let navController: NavController;
+let navParams: NavParams;
+let utilities: Utilities;
+
+describe("UserManagementComponentTest", () =>{
+    beforeEach(()=>{
+        userManagement = new UserManagementComponent(navController, navParams, utilities)
+    })
+
+    it('should be initialzied', () =>{
+        expect(userManagement).toBeTruthy();
+    });
+});
