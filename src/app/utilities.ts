946f1e922ac6e17073ea832ed2c3d178b667030c
Adrian Kochsiek <adrian.kochsiek@hpe.com>
2017-01-24 14:42:04 +0100

    push notification in utilities

--- src/app/utilities.ts
+++ src/app/utilities.ts
@@ -161,6 +161,7 @@ export class Utilities {
       });
     }
   }
+
   sendPushNotification(pushIds: Array<any>, content: String) {
     var notificationObj = { contents: {en: content},
       include_player_ids: pushIds};
