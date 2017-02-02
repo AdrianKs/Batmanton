2c2a78449b4c68db09f82527cce13785abd3b06d
xqtion <andre-h96@gmx.de>
2017-02-02 18:03:30 +0100

    Small Changes

--- src/app/utilities.ts
+++ src/app/utilities.ts
@@ -19,6 +19,7 @@ export class Utilities {
   invitesLoaded: boolean = false;
   allPlayers: Array<any>;
   inRegister: boolean = false;
+  counterOpen: any;
 
   constructor(public alertCtrl: AlertController) {
     this.fireAuth = firebase.auth();
@@ -168,5 +169,23 @@ export class Utilities {
       });
     }
   }
+  
+  countOpen(){
+    firebase.database().ref('clubs/12/invites').once('value', snapshot => {
+      this.counterOpen = 0;
+      let inviteArray = [];
+      let counter = 0;
+      for (let i in snapshot.val()) {
+        if(snapshot.val()[i].recipient == this.user.uid && snapshot.val()[i].state == 0){
+          this.counterOpen++;
+          console.log("+1");
+        }
+      }
+      console.log(this.counterOpen);
+    });
+  }
+
 }
+
+
 
