ebf3058302105b57911a6b9cd69b85d79b310bca
Adrian Kochsiek <adrian.kochsiek@hpe.com>
2017-02-03 17:17:38 +0100

    clubpassword fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -203,13 +203,18 @@ export class MyApp {
 
   loadUserCredentials() {
   let token = window.localStorage.getItem(this.utilities.LOCAL_TOKEN_KEY);
-  if (this.utilities.hashPassword(token) == this.utilities.hashedPassword) {
-    console.log(token);
-    return true;
-  }
-  else{
+  if(token){
+    if (this.utilities.hashPassword(token) == this.utilities.hashedPassword) {
+      console.log(token);
+      return true;
+    } else{
+      console.log('shit - not in the club');
+      return false;
+    }
+  } else {
     console.log('shit - not in the club');
     return false;
   }
+
 }
 }
